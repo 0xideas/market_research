b1631ac4f1fde05920bca642460c7676f38a600a
leon_git <leontimnaluithlen@gmail.com>
2018-08-14 17:24:46 +0200

    clustering completed

--- extract_words.py
+++ extract_words.py
@@ -1,64 +1,6 @@
-for token in doc:
-    if token.dep_ == "nsubj":
-            for child in token.head.children:
-                if child.pos_ in ["ADJ", "VERB"]:
-                    print([child, child.pos_])
-
-
- for token in doc:
-     ...:     if token.dep_ == "nsubj":
-     ...:         for child in token.head.children:
-     ...:             if child.pos_ == "ADJ":
-     ...:                 print("Condition 1")
-     ...:                 print([child, child.pos_])
-     ...:     
-     ...:     if token.dep_ == "dobj":
-     ...:         for child in token.head.children:
-     ...:             if token.head.pos_ == "VERB" and child.dep_ == "nsubj":
-     ...:                 print("Condition 2")
-     ...:                 print([token.head, token.head.pos_])         
-     ...: 
-     ...:     if token.dep_ == "amod" and token.pos_ == "ADJ":
-     ...:         print("Condition 3")
-     ...:         print([token, token.pos_])
-     ...:         
-     ...:     if token.dep_ == "conj" and token.pos_ == "ADJ":
-     ...:         print("Condition 4")
-     ...:         print([token, token.pos_])
-     ...:         
-     ...:     if token.dep_ == "advmod" and token.pos_ == "ADJ":
-     ...:         print([token, token.pos_])
-
-
-for token in doc:
-     ...:     if token.dep_ == "nsubj":
-     ...:         for child in token.head.children:
-     ...:             if child.pos_ == "ADJ":
-     ...:                 print("Condition 1")
-     ...:                 print([child, child.pos_])
-     ...:     
-     ...:     if token.dep_ == "dobj":
-     ...:         print("Condition 2")
-     ...:         print([token.head, token.head.pos_])
-     ...: 
-     ...:     if token.dep_ == "amod" and token.pos_ == "ADJ":
-     ...:         print("Condition 3")
-     ...:         print([token, token.pos_])
-     ...:         
-     ...:     if token.dep_ == "conj" and token.pos_ == "ADJ":
-     ...:         print("Condition 4")
-     ...:         print([token, token.pos_])
-     ...:         
-     ...:     if token.dep_ == "advmod" and token.pos_ == "ADV" and token.head.pos_ =
-     ...: = "ADJ":
-     ...:         print("Condition 5")
-     ...:         print([token, token.pos_])
-     ...:     if token.dep_ == "acomp":
-     ...:         print("Condition 6")
-     ...:         print([token, token.pos_])
-
-
-def extract_words(doc_object):
+def extract_words(text, spacy_object, stop_words):
+    keywords = []
+    doc_object = spacy_object(text)
     for token in doc_object:
         if token.pos_ == "ADJ":
             if token.dep_ in ["amod", "conj"]:
@@ -71,8 +13,20 @@ def extract_words(doc_object):
 
         if token.dep_ in ["dobj", "acomp"]:
             keywords += [token]
+            
+        if token.dep_ == "conj" and token.head.pos_ == "VERB":
+            keywords += [token]
 
         if token.dep_ == "advmod" and token.pos_ == "ADV" and token.head.pos_ == "ADJ":
             keywords += [token]
+        
+        if len([c for c in token.children]) > 3 and token.text not in stop_words:
+            keywords += [token]
+            
+    if len(keywords) == 0:
+        keywords = list(doc_object)
 
-    return(keywords)
+    if len(doc_object) == 0 :
+        keywords = list(spacy_object("and"))
+        
+    return(list(set(keywords)))
